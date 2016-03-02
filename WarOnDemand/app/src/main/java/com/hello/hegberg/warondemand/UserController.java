51289c509fb424f93c43fb474343ba0722540c8f
Julie Wang <juliewang@uws107770usr.uws.ualberta.ca>
2016-03-02 09:49:50 -0700

    user update

--- WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/UserController.java
+++ WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/UserController.java
@@ -1,11 +1,13 @@
 package com.hello.hegberg.warondemand;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class UserController extends AppCompatActivity {
 
@@ -14,14 +16,21 @@ public class UserController extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_controller);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //initalizing all buttons
+        Button addAccount = (Button) findViewById(R.id.addAccount);
+        Button editAccount = (Button) findViewById(R.id.editAccount);
+        addAccount.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AccountController.class));
+
+            }
+        });
+        editAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AccountController.class));
+
             }
         });
     }
