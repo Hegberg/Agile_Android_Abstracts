fa8743afb2a89820f085e465f2f0fdcfb20d0420
Hannah Barlow <hbarlow@ualberta.ca>
2016-03-02 09:16:47 -0700

    Initialized buttons/views in MainActivity

--- WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/MainActivity.java
+++ WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hello.hegberg.warondemand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //initialize buttons
+    Button account = (Button) findViewById(R.id.Account);
+    Button products = (Button) findViewById(R.id.Products);
+    Button help = (Button) findViewById(R.id.Help);
+
+    //when account clicked go to UserController
+    account.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, UserController.class));
+        }
+    };
+
+    //when products is clicked go to <somewhere>
+    products.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //TODO: insert class to go to when 'products' is clicked.
+            startActivity(new Intent(MainActivity.this, <InsertClass>.class));
+        }
+    };
+
+    //when help is clicked go to <somewhere>
+    help.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //TODO: insert class to go to when 'help' is clicked.
+            startActivity(new Intent(MainActivity.this, <InsertClass>.class));
+        }
+    };
+
+
 }
