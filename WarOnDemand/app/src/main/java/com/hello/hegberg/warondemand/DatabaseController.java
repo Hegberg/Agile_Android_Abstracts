95b5ebe47b0b08ad2c9a3adf63a135938d7fe44a
ismailmare <ismailmare@Ismails-MacBook-Air.local>
2016-03-11 01:24:16 -0700

    Database touchup/ App logo

--- WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/DatabaseController.java
+++ WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/DatabaseController.java
@@ -24,86 +24,49 @@ public class DatabaseController {
     private static Gson gson;
 
 
+/* how to use GetUsers/ GetItems:
+------------------------------------
+ElasticsearchTweetController.GetTweetsTask getTweetsTask = new ElasticsearchTweetController.GetTweetsTask();
+        try {
+            getTweetsTask.execute("");
+            tweets = getTweetsTask.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
-//    //TODO: A function that gets tweets
-//    public static class GetInfo extends AsyncTask<String, Void, ArrayList<>> {
-//        // TODO: Get tweets
-//        @Override
-//        protected ArrayList<> doInBackground(String... search_strings) {
-//            verifyClient();
-//
-//            // Base arraylist to hold tweets
-//            //ArrayList<Tweet> tweets = new ArrayList<Tweet>();
-//
-//            String query = "{\n" +
-//                    "\"query\": {\n" +
-//                    "\"term\": { \"tweet\" : \"sad\" }\n" +
-//                    "}\n" +
-//                    "}\n";
-//            String search_string = "";
-//
-//            Search search = new Search.Builder(search_string)
-//                    .addIndex("testing")
-//                    .addType("tweet")
-//                    .build();
-//
-//            try {
-//                SearchResult result = client.execute(search);
-//                if(result.isSucceeded()) {
-//                    //List<NormalTweet> fun = result.getSourceAsObjectList(NormalTweet.class);
-//                    //tweets.addAll(fun);
-//                }
-//            } catch (IOException e) {
-//                // TODO: Something more useful
-//                throw new RuntimeException();
-//            }
-//
-//            return
-//        }
-//
-//
-//    }
-//
-//
-//    public static void addUser(String username){
-//
-//        verifyClient();
-//
-//        Index index = new Index.Builder(username).index("Agile_Android_Abstracts").type("users").build();
-//        try {
-//            DocumentResult result = client.execute(index);
-//            if(result.isSucceeded()) {
-//                // Set the ID to tweet that elasticsearch told me it was
-//                //username.setId(result.getId());
-//            } else {
-//                // TODO: Add an error message, because this was puzzling.
-//                // TODO: Right here it will trigger if the insert fails
-//                Log.i("TODO", "We actually failed here, adding a tweet");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static void addItem(String name){
-//
-//        verifyClient();
-//
-//        Index index = new Index.Builder(name).index("Agile_Android_Abstracts").type("items").build();
-//        try {
-//            DocumentResult result = client.execute(index);
-//            if(result.isSucceeded()) {
-//                // Set the ID to tweet that elasticsearch told me it was
-//                //username.setId(result.getId());
-//            } else {
-//                // TODO: Add an error message, because this was puzzling.
-//                // TODO: Right here it will trigger if the insert fails
-//                Log.i("TODO", "We actually failed here, adding a tweet");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+NormalTweet latestTweet = new NormalTweet(text);
+
+    tweets.add(latestTweet);
+    adapter.notifyDataSetChanged();
+
+    // TODO: Replace with Elasticsearch
+    AsyncTask<NormalTweet, Void, Void> execute = new ElasticsearchTweetController.AddTweetTask();
+    execute.execute(latestTweet);
+    //saveInFile();
+
+    setResult(RESULT_OK);
+/*
+-----------------------------------------
+
+
+How to use AddUser/Additem
+-----------------------------------------
+NormalTweet latestTweet = new NormalTweet(text);
+
+                tweets.add(latestTweet);
+                adapter.notifyDataSetChanged();
+
+                // TODO: Replace with Elasticsearch
+                AsyncTask<NormalTweet, Void, Void> execute = new ElasticsearchTweetController.AddTweetTask();
+                execute.execute(latestTweet);
+                //saveInFile();
+
+                setResult(RESULT_OK);
+ */
+
+
 
 
 
@@ -193,7 +156,7 @@ public class DatabaseController {
             String search_string;
             if(search_strings[0] != "") {
                 //search_string = "{\"from\" : 0, \"size\" : 10000,\"query\":{\"match\":{\"message\":\"" + search_strings[0] + "\"}}}";
-                search_string = "{\"query\":{\"match\":{\"message\":\"" + search_strings[0] + "\"}}}";
+                search_string = "{\"query\":{\"match\":{\"username\":\"" + search_strings[0] + "\"}}}";
             } else {
                 search_string = "{\"from\" : 0, \"size\" : 100}";
             }
@@ -237,7 +200,7 @@ public class DatabaseController {
             String search_string;
             if(search_strings[0] != "") {
                 //search_string = "{\"from\" : 0, \"size\" : 10000,\"query\":{\"match\":{\"message\":\"" + search_strings[0] + "\"}}}";
-                search_string = "{\"query\":{\"match\":{\"message\":\"" + search_strings[0] + "\"}}}";
+                search_string = "{\"query\":{\"match\":{\"name\":\"" + search_strings[0] + "\"}}}";
             } else {
                 search_string = "{\"from\" : 0, \"size\" : 100}";
             }
@@ -266,3 +229,86 @@ public class DatabaseController {
         }
     }
 }
+
+
+
+
+//    //TODO: A function that gets tweets
+//    public static class GetInfo extends AsyncTask<String, Void, ArrayList<>> {
+//        // TODO: Get tweets
+//        @Override
+//        protected ArrayList<> doInBackground(String... search_strings) {
+//            verifyClient();
+//
+//            // Base arraylist to hold tweets
+//            //ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+//
+//            String query = "{\n" +
+//                    "\"query\": {\n" +
+//                    "\"term\": { \"tweet\" : \"sad\" }\n" +
+//                    "}\n" +
+//                    "}\n";
+//            String search_string = "";
+//
+//            Search search = new Search.Builder(search_string)
+//                    .addIndex("testing")
+//                    .addType("tweet")
+//                    .build();
+//
+//            try {
+//                SearchResult result = client.execute(search);
+//                if(result.isSucceeded()) {
+//                    //List<NormalTweet> fun = result.getSourceAsObjectList(NormalTweet.class);
+//                    //tweets.addAll(fun);
+//                }
+//            } catch (IOException e) {
+//                // TODO: Something more useful
+//                throw new RuntimeException();
+//            }
+//
+//            return
+//        }
+//
+//
+//    }
+//
+//
+//    public static void addUser(String username){
+//
+//        verifyClient();
+//
+//        Index index = new Index.Builder(username).index("Agile_Android_Abstracts").type("users").build();
+//        try {
+//            DocumentResult result = client.execute(index);
+//            if(result.isSucceeded()) {
+//                // Set the ID to tweet that elasticsearch told me it was
+//                //username.setId(result.getId());
+//            } else {
+//                // TODO: Add an error message, because this was puzzling.
+//                // TODO: Right here it will trigger if the insert fails
+//                Log.i("TODO", "We actually failed here, adding a tweet");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void addItem(String name){
+//
+//        verifyClient();
+//
+//        Index index = new Index.Builder(name).index("Agile_Android_Abstracts").type("items").build();
+//        try {
+//            DocumentResult result = client.execute(index);
+//            if(result.isSucceeded()) {
+//                // Set the ID to tweet that elasticsearch told me it was
+//                //username.setId(result.getId());
+//            } else {
+//                // TODO: Add an error message, because this was puzzling.
+//                // TODO: Right here it will trigger if the insert fails
+//                Log.i("TODO", "We actually failed here, adding a tweet");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
