55509671008d74e37917cba818e16370ae001fdf
Christopher Hegberg <hegberg@cs.ualberta.ca>
2016-03-29 17:55:54 -0600

    fixed login stuff

--- WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/BiddingActivity.java
+++ WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/BiddingActivity.java
@@ -1,20 +1,13 @@
 package com.hello.hegberg.warondemand;
 
-import android.app.TaskStackBuilder;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.support.v7.app.ActionBarActivity;
+
 
 
 import java.util.ArrayList;
