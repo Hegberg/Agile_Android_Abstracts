be1b2f321df00182036944c05a2cad4d9a9e6487
Koukoula <unknown.essence@hotmail.com>
2016-04-04 07:53:11 -0600

    Implemented default images

--- WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/WarItemAdapter.java
+++ WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/WarItemAdapter.java
@@ -29,10 +29,10 @@ public class WarItemAdapter extends ArrayAdapter<WarItem>{
         // Get the data item for this position
         WarItem warItem = getItem(position);
         String message = warItem.toString();
-
+        Bitmap thumbnail;
         //TODO: initialize this:
 //       Bitmap thumbnail = null;
-        Bitmap thumbnail = warItem.getThumbnail();
+
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
@@ -43,7 +43,14 @@ public class WarItemAdapter extends ArrayAdapter<WarItem>{
         tweetText.setText(message);
 //       if(thumbnail != null) {
         // Only add it if it's null.
-        tweetImage.setImageBitmap(thumbnail);
+
+        //I was not sure on how to make a default image, so this is my attempt.
+        //Default image is whatever is contained in the src of the xml.
+        if(warItem.getThumbnail() != null) {
+            thumbnail = warItem.getThumbnail();
+            tweetImage.setImageBitmap(thumbnail);
+
+        }
 //       }
 
         // Return the completed view to render on screen
