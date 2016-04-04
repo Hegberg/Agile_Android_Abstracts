a6e1f0998f962efae39805ad8e17a1fe7954b788
Whitishmeteor <hegburgers-are-fat-free@live.ca>
2016-04-03 23:47:53 -0600

    fixed looking at borrowed objects

--- WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/ViewWarItemNoEdit.java
+++ WarOnDemand/app/src/main/java/com/hello/hegberg/warondemand/ViewWarItemNoEdit.java
@@ -2,6 +2,7 @@ package com.hello.hegberg.warondemand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewWarItemNoEdit extends AppCompatActivity {
 
@@ -10,6 +11,19 @@ public class ViewWarItemNoEdit extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_war_item_no_edit);
 
+        TextView itemsName = (TextView) findViewById(R.id.name_info_view_borrower);
+        TextView itemsDesc = (TextView) findViewById(R.id.desc_view_borrower);
+        TextView biddersName = (TextView) findViewById(R.id.borrower_name_view_borrower);
+        TextView minBidPrice = (TextView) findViewById(R.id.min_bid_price_borrowing);
 
+        itemsName.setText(ViewMyItemsActivity.itemClicked.getName());
+        itemsDesc.setText(ViewMyItemsActivity.itemClicked.getDesc());
+        minBidPrice.setText(String.valueOf(ViewMyItemsActivity.itemClicked.getCost()));
+        try {
+            biddersName.setText(ViewMyItemsActivity.itemClicked.getBorrower().getUsername());
+        } catch (NullPointerException e) {
+
+            biddersName.setText("No current borrower");
+        }
     }
 }
